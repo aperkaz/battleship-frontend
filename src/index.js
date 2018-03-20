f6540b4c27ddd1fd7ffd384b6c72d6bcd09ab54e
Alain Perkaz <alainperkaz@gmail.com>
2018-03-20 15:48:58 +0100

    Update store rules and margin

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 startRouter(views, store);
 
-
 ReactDOM.render(
     <MobxProvider store={store}>
-        <div style={{ textAlign: 'center'}}>
+        <div style={{ textAlign: 'center', marginTop: '20px'}}>
             <h1>{store.game.title}</h1>
             <MobxRouter/>
         </div>
--- src/mobx/store.js
+++ src/mobx/store.js
@@ -9,14 +9,9 @@ import PreparationStore from './stores/preparationStore';
 const title = 'Battleship';
 const players = ['player1', 'player2'];
 export const rules = {
-    boardWidth: 3,
-    ships: [[2,1], [2,1]],
-};
-
-/*
-boardWidth: 10,
+    boardWidth: 10,
     ships: [[5,1], [4,1], [3,1], [2,2]],
-* */
+};
 
 class RootStore{
     constructor() {
